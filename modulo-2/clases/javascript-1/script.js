f947099529198d46aa3d81701aa4e1de88e13ce1
Primo18 <danilobo.elo@gmail.com>
2024-03-28 18:45:32 -0300

    add code m2

---
+++ modulo-2/clases/javascript-1/script.js
@@ -0,0 +1,12 @@
+let num1 = +prompt('Ingrese un número');
+let num2 = +prompt('Ingrese otro número');
+
+let result = num1 + num2;
+let resultado = parseInt(num1) + parseInt(num2);
+
+console.log(result);
+console.log(resultado);
+
+document.write(result);
+document.write('<br>');
+document.write(resultado);
